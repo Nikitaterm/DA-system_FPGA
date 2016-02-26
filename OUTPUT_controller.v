eebdc7cba2e7edf0a390bb9d210cc1345072d66b
Nikita Lazarev <nikitaterm@gmail.com>
2016-02-26 14:14:59 +0300

    Fixed time_counter: right implementation

--- OUTPUT_controller.v
+++ OUTPUT_controller.v
@@ -1,7 +1,7 @@
 module OUTPUT_controller(input clock, input [10:0] data_in, input[3:0] avaliable_data, 
 								 output fifo_read_clock, output fifo_read_irq, output[10:0] data_out, output IRQ);
-
-	assign data_out = data_in;
+	reg[10:0] data_ = 0;
+	assign data_out = data_;
 	reg[7:0] state = 0;
 	reg irq = 0;
 	assign IRQ = irq;
@@ -15,7 +15,7 @@ module OUTPUT_controller(input clock, input [10:0] data_in, input[3:0] avaliable
 			fifo_read_clk <= 1;
 			state <= state + 1;
 		end
-		if (state == 1) begin
+		if (state == 1) begin	// Is the condition being checked by the next clock?
 			fifo_read_clk <= 0;
 			state <= state + 1;
 		end
@@ -30,15 +30,16 @@ module OUTPUT_controller(input clock, input [10:0] data_in, input[3:0] avaliable
 		end
 		if (state == 3) begin
 			fifo_read_clk <= 1;
-			irq <= 1;
 			state <= state + 1;
 		end
-		if (state >= 4 && state < 250) begin
+		if (state >= 4 && state < 40) begin
+		   data_ <= data_in;
+			irq <= 1;
 		   fifo_read_clk <= 0;
 		   fifo_read_request <= 0;
 			state <= state + 1;
 		end
-		if (state == 250) begin
+		if (state == 40) begin
 			irq <= 0;
 			state <= 0;
 		end
--- time_counter.v
+++ time_counter.v
@@ -1,5 +1,4 @@
 module time_counter(input A, input B, input clock, output[10:0] _time, output to_fifo);
-	
 	reg erase = 0;
 	reg prev_A = 0;
 	reg[9:0] count = 0;
@@ -9,25 +8,31 @@ module time_counter(input A, input B, input clock, output[10:0] _time, output to
 	assign to_fifo = _to_fifo;
 
 	always @(posedge clock) begin
+		//if (count == 5) begin
 		if (A != prev_A) begin
 			if (A) begin
-			   _time_data <= count;
+				_time_data <= count;
+				//_time_data <= _time_data + 1;
 				_to_fifo <= 1;
 				erase <= 1;
 			end
 			prev_A <= A;
 		end
+		count <= count + 1;
 		if (erase == 1) begin
 			count <= 0;
+			erase <= 0;
 		end
-		if (count == 0) begin
+		if (count == 10) begin
 	      _to_fifo <= 0;
+			//count <= 0;
 		end
 		if (count == 10'b1111111111) begin
          _time_data <= count | (1<<10);
+		   //_time_data <= _time_data + 1;
 			_to_fifo <= 1;
+			erase <= 1;
 		end
-		count <= count + 1;
-	end
 
+	end
 endmodule
